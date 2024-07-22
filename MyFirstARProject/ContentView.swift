999753c1a0925fc37269d6a927de28fe17fdc92a
Dimitar Anguelov <d.angelov.9090@gmail.com>
2024-07-22 13:27:58 +0300

    Initial Commit

---
+++ MyFirstARProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyFirstARProject
+//
+//  Created by Dimitar Angelov on 22.07.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyFirstARProject/MyFirstARProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstARProjectApp.swift
+//  MyFirstARProject
+//
+//  Created by Dimitar Angelov on 22.07.24.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstARProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
